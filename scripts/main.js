a53d931352fbc9a04586199537adbba42ced0d39
Sam H <sam.huelsdonk@gmail.com>
2017-09-01 19:55:13 -0500

    data records added/removed for files (file in cloud is left for now)

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Shortcuts to DOM Elements.
-var messageForm = document.getElementById('message-form');
-var messageInput = document.getElementById('new-post-message');
-var titleInput = document.getElementById('new-post-title');
 var signInButton = document.getElementById('sign-in-button');
 var signOutButton = document.getElementById('sign-out-button');
 var loginPage = document.getElementById('login-page');
@@ -12,27 +8,25 @@ var templatesList = document.getElementById('templates-list');
 var postsContainer = document.getElementById('posts-container');
 var uploadModel = document.getElementById('upload-model');
 var modelUpload = document.getElementById('model-upload');
+var fileTitle = document.getElementById('file-title');
 var listeningFirebaseRefs = [];
 
-function writeNewPost(uid, username, picture, title, body) {
-  // A post entry.
+function writeNewPost(postKey, uid, username, picture, title, fileSize, meta) {
   var postData = {
     author: username,
     uid: uid,
-    body: body,
     title: title,
-    starCount: 0,
-    authorPic: picture
+    authorPic: picture,
+    size: fileSize,
+    meta: meta
   };
 
-  var newPostKey = firebase.database().ref().child('posts').push().key;
   var updates = {};
-  updates['/posts/' + newPostKey] = postData;
-  updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+  updates['/posts/' + postKey] = postData;
+  updates['/user-posts/' + uid + '/' + postKey] = postData;
 
   return firebase.database().ref().update(updates);
 }
-
 function createPostElement(postId, title, text, author, authorId, authorPic) {
   var uid = firebase.auth().currentUser.uid;
 
@@ -40,10 +34,10 @@ function createPostElement(postId, title, text, author, authorId, authorPic) {
     '<div id="model-' + postId + '" class="model-item">' +
     '<div class="model-title"></div>' +
     '<div class="model-avatar"></div>' +
-    '<div class="model-username"></div>'
+    '<div class="model-username"></div>' +
+    '<button class="model-remove">Remove</button>'
   '</div>';
 
-  // Create the DOM element from the HTML.
   var div = document.createElement('div');
   div.innerHTML = html;
   var postElement = div.firstChild;
@@ -52,15 +46,17 @@ function createPostElement(postId, title, text, author, authorId, authorPic) {
   postElement.getElementsByClassName('model-username')[0].innerText = author || 'Anonymous';
   postElement.getElementsByClassName('model-avatar')[0].style.backgroundImage = 'url("' +
     (authorPic || './silhouette.jpg') + '")';
-
+  postElement.getElementsByClassName('model-remove')[0].addEventListener('click', function(e) {
+    var updates = {};
+    updates['/posts/' + postId] = null;
+    updates['/user-posts/' + uid + '/' + postId] = null;
+    firebase.database().ref().update(updates).then(function(e) {
+      //live data will refresh itself
+    });
+  });
   return postElement;
 }
-
-/**
- * Starts listening for new posts and populates posts lists.
- */
 function startDatabaseQueries() {
-  // [START my_top_posts_query]
   var myUserId = firebase.auth().currentUser.uid;
   var recentPostsRef = firebase.database().ref('posts').limitToLast(100);
 
@@ -77,8 +73,9 @@ function startDatabaseQueries() {
       postElement.getElementsByClassName('model-username')[0].innerText = data.val().author;
     });
     postsRef.on('child_removed', function(data) {
-      var post = postsContainer.getElementsByClassName('post-' + data.key)[0];
-      post.parentElement.removeChild(post);
+      var post = document.getElementById('model-' + data.key);
+      if (post)
+        postsContainer.removeChild(post);
     });
   };
 
@@ -86,7 +83,6 @@ function startDatabaseQueries() {
 
   listeningFirebaseRefs.push(recentPostsRef);
 }
-
 function writeUserData(userId, name, email, imageUrl) {
   firebase.database().ref('users/' + userId).set({
     username: name,
@@ -94,31 +90,17 @@ function writeUserData(userId, name, email, imageUrl) {
     profile_picture: imageUrl
   });
 }
-// [END basic_write]
-
-/**
- * Cleanups the UI and removes all Firebase listeners.
- */
 function cleanupUi() {
-  // Remove all previously displayed posts.
   postsContainer.innerHTML = '';
 
-  // Stop all currently listening Firebase listeners.
   listeningFirebaseRefs.forEach(function(ref) {
     ref.off();
   });
   listeningFirebaseRefs = [];
 }
 
-/**
- * The ID of the currently signed-in User. We keep track of this to detect Auth state change events that are just
- * programmatic token refresh but not a User status change.
- */
 var currentUID;
 
-/**
- * Triggers every time there is a change in the Firebase auth state (i.e. user signed-in or user signed out).
- */
 function onAuthStateChanged(user) {
   // We ignore token refresh events.
   if (user && currentUID === user.uid) {
@@ -140,58 +122,15 @@ function onAuthStateChanged(user) {
     mainPage.style.display = 'none';
   }
 }
-
-/**
- * Creates a new post for the current user.
- */
-function newPostForCurrentUser(title, text) {
-  // [START single_value_read]
+function newPostForCurrentUser(postKey, title, fileUrl, totalBytes, meta) {
   var userId = firebase.auth().currentUser.uid;
   return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
     var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
-    // [START_EXCLUDE]
-    return writeNewPost(firebase.auth().currentUser.uid, username,
+    return writeNewPost(postKey, firebase.auth().currentUser.uid, username,
       firebase.auth().currentUser.photoURL,
-      title, text);
-    // [END_EXCLUDE]
+      title, fileUrl, totalBytes, meta);
   });
-  // [END single_value_read]
 }
-
-// Bindings on load.
-window.addEventListener('load', function() {
-  // Bind Sign in button.
-  signInButton.addEventListener('click', function() {
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
-  });
-
-  // Bind Sign out button.
-  signOutButton.addEventListener('click', function() {
-    firebase.auth().signOut();
-  });
-
-  // Listen for auth state changes
-  firebase.auth().onAuthStateChanged(onAuthStateChanged);
-
-  // Saves message on form submit.
-  messageForm.onsubmit = function(e) {
-    e.preventDefault();
-    var text = messageInput.value;
-    var title = titleInput.value;
-    if (text && title) {
-      newPostForCurrentUser(title, text).then(function() {
-        myPostsMenuButton.click();
-      });
-      messageInput.value = '';
-      titleInput.value = '';
-    }
-  };
-
-  modelUpload.addEventListener('change', handleFileSelect, false);
-  //modelUpload.disabled = true;
-}, false);
-
 function handleFileSelect(evt) {
   evt.stopPropagation();
   evt.preventDefault();
@@ -202,42 +141,60 @@ function handleFileSelect(evt) {
   var metadata = {
     'contentType': file.type
   };
+
   storageRef.child('images/' + file.name).put(file, metadata).then(function(snapshot) {
-    console.log('Uploaded', snapshot.totalBytes, 'bytes.');
-    console.log(snapshot.metadata);
-    var url = snapshot.downloadURL;
-    console.log('File available at', url);
-    // [START_EXCLUDE]
-    document.getElementById('linkbox').innerHTML = '<a href="' + url + '">Click For File</a>';
-    // [END_EXCLUDE]
+
+    var newPostKey = firebase.database().ref().child('posts').push().key;
+    var title = fileTitle.value.trim();
+    if (title === '')
+      title = new Date().toISOString();
+
+    newPostForCurrentUser(newPostKey, title, snapshot.downloadURL, snapshot.totalBytes, snapshot.metadata)
+      .then(function(e) {
+        fileTitle.value = '';
+        evt.target.value = '';
+      });
   }).catch(function(error) {
-    // [START onfailure]
     console.error('Upload failed:', error);
-    // [END onfailure]
   });
-  // [END oncomplete]
+}
+function initCanvas() {
+  var canvas = document.getElementById("renderCanvas");
+  var engine = new BABYLON.Engine(canvas, true);
+  var createScene = function() {
+    var scene = new BABYLON.Scene(engine);
+    scene.clearColor = new BABYLON.Color3(0, 1, 0);
+    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+    camera.setTarget(BABYLON.Vector3.Zero());
+    camera.attachControl(canvas, false);
+    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+    light.intensity = .5;
+    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+    sphere.position.y = 1;
+    var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+    return scene;
+
+  };
+  var scene = createScene();
+  engine.runRenderLoop(function() {
+    scene.render();
+  });
 }
 
-var canvas = document.getElementById("renderCanvas");
-var engine = new BABYLON.Engine(canvas, true);
-var createScene = function() {
-  var scene = new BABYLON.Scene(engine);
-  scene.clearColor = new BABYLON.Color3(0, 1, 0);
-  var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-  camera.setTarget(BABYLON.Vector3.Zero());
-  camera.attachControl(canvas, false);
-  var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-  light.intensity = .5;
-  var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-  sphere.position.y = 1;
-  var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
-  return scene;
-
-};
-var scene = createScene();
-engine.runRenderLoop(function() {
-  scene.render();
-});
+initCanvas();
 window.addEventListener("resize", function() {
   engine.resize();
 });
+window.addEventListener('load', function() {
+  // Bind Sign in button.
+  signInButton.addEventListener('click', function() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  });
+
+  signOutButton.addEventListener('click', function() {
+    firebase.auth().signOut();
+  });
+  firebase.auth().onAuthStateChanged(onAuthStateChanged);
+  modelUpload.addEventListener('change', handleFileSelect, false);
+}, false);
