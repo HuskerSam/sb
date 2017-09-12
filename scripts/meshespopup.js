54e9c492930936ea171353e1743fac92d797c113
Sam H <sam.huelsdonk@gmail.com>
2017-09-12 12:45:31 -0500

    fixed up tab rotate positions, tabs, sizing, etc for mesh popup

--- scripts/meshespopup.js
+++ scripts/meshespopup.js
@@ -14,8 +14,12 @@ meshespopup.init = function() {
   this.meshCanvas = document.getElementById('meshDetailCanvas');
   this.detailsContainer = document.getElementById('details-content-div');
   this.babyHelper = new BabylonHelper('meshDetailCanvas');
+  this.detailTabButton = document.getElementById('mesh-detail-tab-button');
+  this.jsonTabButton = document.getElementById('mesh-json-tab-button');
+  this.babyTabButton = document.getElementById('mesh-babylon-tab-button');
 
   this.tabHeight = 200;
+  this.tabWidth = 200;
   this.tabRotate = false;
 
   this.setTabDragLimits();
@@ -31,6 +35,10 @@ meshespopup.rotateTab = function(e) {
     meshespopup.portraitLowerCell.appendChild(meshespopup.detailsContainer);
     meshespopup.portraitUpperCell.appendChild(meshespopup.meshCanvas);
     meshespopup.tabRotate = false;
+
+    meshespopup.detailTabButton.innerHTML = 'Detail';
+    meshespopup.jsonTabButton.innerHTML = 'JSON';
+    meshespopup.babyTabButton.innerHTML = 'Babylon';
   } else {
     meshespopup.tabRotate = true;
     meshespopup.portraitLowerRow.style.display = 'none';
@@ -39,25 +47,43 @@ meshespopup.rotateTab = function(e) {
 
     meshespopup.landscapeLeftCell.appendChild(meshespopup.meshCanvas);
     meshespopup.landscapeRightCell.appendChild(meshespopup.detailsContainer);
+
+    meshespopup.detailTabButton.innerHTML = 'D';
+    meshespopup.jsonTabButton.innerHTML = 'J';
+    meshespopup.babyTabButton.innerHTML = 'B';
   }
 };
 meshespopup.setTabDragLimits = function() {
   meshespopup.tabHeightMax = window.innerHeight - 300;
   meshespopup.tabHeightMin = 100;
+  meshespopup.tabWidthMax = window.innerWidth - 300;
+  meshespopup.tabWidthMin = 50;
 };
 meshespopup.resizeMouseDown = function(e) {
   meshespopup.dragButtonStartY = e.clientY;
+  meshespopup.dragButtonStartX = e.clientX;
+
   window.addEventListener('mousemove', meshespopup.resizeMouseMove, false);
   window.addEventListener('mouseup', meshespopup.resizeMouseUp, false);
 };
 meshespopup.resizeMouseMove = function(e) {
-  meshespopup.tabHeight = meshespopup.tabHeight + (meshespopup.dragButtonStartY - e.clientY);
-  meshespopup.dragButtonStartY = e.clientY;
-  if (meshespopup.tabHeight > meshespopup.tabHeightMax)
-    meshespopup.tabHeight = meshespopup.tabHeightMax;
-  if (meshespopup.tabHeight < meshespopup.tabHeightMin)
-    meshespopup.tabHeight = meshespopup.tabHeightMin;
-  meshespopup.meshesTab.style.height = meshespopup.tabHeight + 'px';
+  if (meshespopup.tabRotate) {
+    meshespopup.tabWidth = meshespopup.tabWidth + (meshespopup.dragButtonStartX - e.clientX);
+    meshespopup.dragButtonStartX = e.clientX;
+    if (meshespopup.tabWidth > meshespopup.tabWidthMax)
+      meshespopup.tabWidth = meshespopup.tabWidthMax;
+    if (meshespopup.tabWidth < meshespopup.tabWidthMin)
+      meshespopup.tabWidth = meshespopup.tabWidthMin;
+    meshespopup.meshesTab.style.width = meshespopup.tabWidth + 'px';
+  } else {
+    meshespopup.tabHeight = meshespopup.tabHeight + (meshespopup.dragButtonStartY - e.clientY);
+    meshespopup.dragButtonStartY = e.clientY;
+    if (meshespopup.tabHeight > meshespopup.tabHeightMax)
+      meshespopup.tabHeight = meshespopup.tabHeightMax;
+    if (meshespopup.tabHeight < meshespopup.tabHeightMin)
+      meshespopup.tabHeight = meshespopup.tabHeightMin;
+    meshespopup.meshesTab.style.height = meshespopup.tabHeight + 'px';
+  }
 };
 meshespopup.resizeMouseUp = function(e) {
   window.removeEventListener('mousemove', meshespopup.resizeMouseMove, false);
