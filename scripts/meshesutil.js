efe7b9fc6cc084b7ffcbb611fc39cc51aa7868f3
Sam H <sam.huelsdonk@gmail.com>
2017-09-22 02:30:03 -0500

    transfer

--- scripts/meshesutil.js
+++ scripts/meshesutil.js
@@ -20,48 +20,7 @@ var defaultMeshData = {
 };
 
 meshes.init = function() {
-  this.dialog = document.getElementById('model-upload-dialog');
-  this.objIdUpload = document.getElementById('object-id');
-  this.fileUpload = document.getElementById('model-upload-file');
-  this.dialogCloseUpload = this.dialog.querySelector('.close');
-  this.dialogShowButton = document.getElementById('upload-button');
-  this.dialogUploadButton = this.dialog.querySelector('.import');
-  this.dialogProgress = document.getElementById('model-upload-progress');
+
 
   var me = this;
-  if (!this.dialog.showModal) {
-    dialogPolyfill.registerDialog(this.dialog);
-  }
-  this.dialogShowButton.addEventListener('click', function() {
-    me.dialogCloseUpload.style.display = '';
-    me.dialogUploadButton.style.display = '';
-    me.dialogProgress.style.display = 'none';
-    me.dialog.showModal();
-  });
-  this.dialogCloseUpload.addEventListener('click', function() {
-    me.dialog.close();
-  });
-  this.dialogUploadButton.addEventListener('click', function() {
-    var file = me.fileUpload.files[0];
-    var objId = me.objIdUpload.value.trim();
-    if (!objId) {
-      alert('Need an id to import');
-      return;
-    }
-    me.dialogCloseUpload.style.display = 'none';
-    me.dialogUploadButton.style.display = 'none';
-    me.dialogProgress.style.display = '';
-    babyUtil.fileToURL(file).then(function(fileData) {
-      babyUtil.serializeMesh(objId, "", "data:" + fileData).then(function(meshJSON) {
-        fireUtil.newModel(JSON.stringify(meshJSON), objId).then(function(result) {
-          me.objIdUpload.value = '';
-          me.fileUpload.value = '';
-          me.dialogCloseUpload.style.display = '';
-          me.dialogUploadButton.style.display = '';
-          me.dialogProgress.style.display = 'none';
-          me.dialog.close();
-        });
-      });
-    });
-  });
 };
--- scripts/scenebuilder.js
+++ scripts/scenebuilder.js
@@ -7,20 +7,73 @@ scenebuilder.init = function() {
   this.skinsCollapseButton = document.getElementById('collapse-skins');
   this.skinsCollapsePanel = document.getElementById('skins-content');
 
+  this.initMeshUpload();
   this.meshesCollapseButton.addEventListener('click',
     (e) => me.toggleBar(me.meshesCollapseButton, me.meshesCollapsePanel), false);
 
   this.skinsCollapseButton.addEventListener('click',
     (e) => me.toggleBar(me.skinsCollapseButton, me.skinsCollapsePanel), false);
 };
+scenebuilder.initMeshUpload = function() {
+  let me = this;
+  this.showMeshUploadDialog = document.getElementById('mesh-upload-button');
+  this.meshUploadDialog = document.getElementById('mesh-upload-dialog');
+  this.meshUploadFileDom = document.getElementById('mesh-upload-file');
+  this.meshObjIdUpload = document.getElementById('mesh-object-id');
+  this.importMeshDialogProgress = document.getElementById('mesh-upload-progress');
+  this.importMeshUploadButton = this.meshUploadDialog.querySelector('.import');
+  this.meshUploadDialogClose = this.meshUploadDialog.querySelector('.close');
+
+  if (!this.meshUploadDialog.showModal) {
+    dialogPolyfill.registerDialog(this.meshUploadDialog);
+  }
+  this.showMeshUploadDialog.addEventListener('click', function() {
+    me.meshUploadDialogClose.style.display = '';
+    me.importMeshUploadButton.style.display = '';
+    me.importMeshDialogProgress.style.display = 'none';
+    me.meshUploadDialog.showModal();
+  });
+  this.meshUploadDialogClose.addEventListener('click', function() {
+    me.meshUploadDialog.close();
+  });
+
+  this.importMeshUploadButton.addEventListener('click', (e) => me.uploadMesh(), false);
+};
+scenebuilder.uploadMesh = function() {
+  let me = this;
+  let file = me.meshUploadFileDom.files[0];
+  let objId = me.meshObjIdUpload.value.trim();
+  if (!objId) {
+    alert('Need an id to import');
+    return;
+  }
+  me.meshUploadDialogClose.style.display = 'none';
+  me.importMeshUploadButton.style.display = 'none';
+  me.importMeshDialogProgress.style.display = '';
+  babyUtil.fileToURL(file).then(function(fileData) {
+    babyUtil.serializeMesh(objId, "", "data:" + fileData).then(function(meshJSON) {
+      fireUtil.newModel(JSON.stringify(meshJSON), objId).then(function(result) {
+        me.meshObjIdUpload.value = '';
+        me.meshUploadFileDom.value = '';
+        me.meshUploadDialogClose.style.display = '';
+        me.importMeshUploadButton.style.display = '';
+        me.importMeshDialogProgress.style.display = 'none';
+        me.meshUploadDialog.close();
+      });
+    });
+  });
+};
+
 scenebuilder.toggleBar = function(button, bar) {
   if (bar.style.display === 'none') {
     bar.style.display = '';
     button.querySelector('i').innerHTML = 'expand_more';
     bar.parentNode.style.display = '';
+    bar.parentNode.parentNode.insertBefore(bar.parentNode, bar.parentNode.parentNode.childNodes[0]);
   } else {
     bar.style.display = 'none';
     bar.parentNode.style.display = 'inline-block';
     button.querySelector('i').innerHTML = 'expand_less';
+    bar.parentNode.parentNode.insertBefore(bar.parentNode, null);
   }
 };
