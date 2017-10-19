7cc26fea180187c0cbb8e37ead2c2fe972645d4a
Sam H <sam.huelsdonk@gmail.com>
2017-10-19 15:08:36 -0500

    firefields display for frames

--- controller/cbandframes.js
+++ controller/cbandframes.js
@@ -1,14 +1,39 @@
 class cBandFrames extends cBandSuper {
   constructor(childrenContainer) {
     super(gAPPP.a.modelSets['frame'], 'frame');
-
+    this.fireSet = gAPPP.a.modelSets['frame'];
     this.childrenContainer = childrenContainer;
+    this.frameDataViewInstances = {};
   }
   _getDomForChild(fireData) {
     let values = fireData.val();
     let key = fireData.key;
-    let dd = document.createElement('div');
-    dd.innerHTML = 'frames div';
-    this.childrenContainer.insertBefore(dd, this.childrenContainer.firstChild);
+    let framesContainer = document.createElement('div');
+
+    let instance = {};
+    instance.frameFields = sDataDefinition.bindingFieldsCloned('frame');
+    instance.key = key;
+    instance.tag = 'frame';
+    instance.fireSet = this.fireSet;
+    instance.dataPanel = new cPanelData(instance.frameFields, framesContainer, instance);
+    instance.childListener = (values, type, fireData) => instance.dataPanel._handleDataChange(values, type, fireData);
+    this.fireSet.childListeners.push(instance.childListener);
+    this.frameDataViewInstances[key] = instance;
+
+    this.childrenContainer.appendChild(framesContainer);
+  }
+  clearChildren() {
+    for (let i in this.frameDataViewInstances) {
+      let fd = this.frameDataViewInstances[i];
+      for (let c = 0, l = this.fireSet.childListeners.length; c < l; c++) {
+        if (this.fireSet.childListeners[c] === fd.childListener) {
+          this.fireSet.childListeners.splice(c, 1);
+          break;
+        }
+      }
+    }
+
+    this.frameDataViewInstances = {};
+    super.clearChildren();
   }
 }
