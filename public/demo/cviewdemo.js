7b37ad01db7ed752d481094731285c1e38a2f5b0
HuskerSam <sam.huelsdonk@gmail.com>
2018-08-17 09:55:48 -0500

    demo view start

---
+++ public/demo/cviewdemo.js
@@ -0,0 +1,72 @@
+class cViewDemo extends bView {
+  constructor() {
+    super();
+
+    this.elementSelect = document.getElementById('element-type-to-edit');
+    this.elementSelect.addEventListener('input', e => this.elementTypeChange());
+    this.blockId = document.getElementById('element-id-to-edit');
+    this.blockField = document.getElementById('field-name-to-edit');
+    this.fieldValue = document.getElementById('value-to-edit');
+    this.setButton = document.getElementById('button-to-edit');
+    this.setButton.addEventListener('click', e => this.setValue());
+
+    this.bandButtons = [];
+    this.fontToolsContainer = this.dialog.querySelector('#publish-profile-panel');
+    this.fontFields = sDataDefinition.bindingFieldsCloned('publishFontFamilyProfile');
+    this.fontFieldsContainer = this.fontToolsContainer.querySelector('.fields-container');
+    this.fontToolsButton = this.dialog.querySelector('#publish-settings-button');
+    this.fontTools = new cBandProfileOptions(this.fontToolsButton, this.fontFields, this.fontFieldsContainer, this.fontToolsContainer);
+    this.fontTools.fireFields.values = gAPPP.a.profile;
+    this.fontTools.activate();
+    this.bandButtons.push(this.fontTools);
+    this.fontTools.closeOthersCallback = () => this.closeHeaderBands();
+
+    this.dialog.querySelector('#user-profile-dialog-reset-button').addEventListener('click', e =>{
+      gAPPP.a.resetProfile();
+      setTimeout(() => location.reload(), 100);
+    });
+
+    this.elementTypeChange();
+  }
+  closeHeaderBands() {
+
+  }
+  setValue() {
+    let t = this.elementSelect.value.toLowerCase();
+    let id = this.blockId.value.split(' ')[0].trim();
+    let field = this.blockField.value.trim();
+    let v = this.fieldValue.value;
+
+    if (id === '')
+      return;
+
+    if (field === '')
+      return;
+
+    gAPPP.a.modelSets[t].commitUpdateList([{
+      field: field,
+      newValue: v
+    }], id);
+
+  }
+  elementTypeChange() {
+    let t = this.elementSelect.value.toLowerCase();
+
+    let options = '';
+    let fS = gAPPP.a.modelSets[t].fireDataValuesByKey;
+    for (let i in fS)
+      options += '<option>' + i + ' ' + fS[i].title + '</option>';
+
+    document.getElementById('elementidlist').innerHTML = options;
+
+    if (t === 'frame')
+      t = 'shapeFrame';
+    let fields = sDataDefinition.bindingFields(t);
+
+    let fieldOptions = '';
+    for (let c = 0, l = fields.length; c< l; c++)
+      fieldOptions += '<option>' + fields[c].fireSetField + '</option>';
+
+    document.getElementById('fieldnamelist').innerHTML = fieldOptions;
+  }
+}
---
+++ public/demo/gdemoapp.js
@@ -0,0 +1,41 @@
+'use strict';
+class gDemoApp extends gAppSuper  {
+  constructor() {
+    super();
+    this.a = new gAuthorization();
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
+    this.a.signInAnon();
+  }
+  profileReadyAndLoaded() {
+    this.loadStarted = true;
+
+    let urlParams = new URLSearchParams(window.location.search);
+    let workspace = urlParams.get('w');
+    let block = urlParams.get('b');
+    let workspaceCode = urlParams.get('z');
+    let blockCode = urlParams.get('y');
+
+    if (workspaceCode) {
+      let data = gAPPP.a.modelSets['projectTitles'].getValuesByFieldLookup('code', workspaceCode);
+      if (data)
+        workspace = gAPPP.a.modelSets['projectTitles'].lastKeyLookup;
+    }
+    this.a.profile.selectedWorkspace = workspace;
+    this.a.initProjectModels(workspace);
+
+    this.a._activateModels();
+    this.initialUILoad = false;
+
+    gAPPP.a.workspaceLoadedCallback = () => {
+      if (blockCode) {
+        let data = gAPPP.a.modelSets['block'].getValuesByFieldLookup('blockCode', blockCode);
+        if (data)
+          block = gAPPP.a.modelSets['block'].lastKeyLookup;
+      }
+
+      gAPPP.a.profile['selectedBlockKey' + workspace] = block;
+      this.mV = new cViewDemo();
+      this._updateApplicationStyle();
+    };
+  }
+}
