b2ada3621874a04da91e0503c603e06b100df574
Sam H <sam.huelsdonk@gmail.com>
2018-08-27 11:10:00 -0500

    transfer

--- public/demo/cviewdemo.js
+++ public/demo/cviewdemo.js
@@ -6,6 +6,7 @@ class cViewDemo extends bView {
         this.canvasHelper.cameraSelect.selectedIndex = 2;
         this.canvasHelper.cameraChangeHandler();
         this.canvasHelper.playAnimation();
+        this.updateProducts();
       }, 200);
     };
 /*
@@ -19,5 +20,120 @@ class cViewDemo extends bView {
   }
   closeHeaderBands() {
 
+
+  }
+  updateProducts() {
+    if (this.productsUpdated)
+      return;
+
+    this.productsUpdated = true;
+    let bD = gAPPP.a.modelSets['block'].fireDataValuesByKey;
+    this.products = [];
+    for (let i in bD) {
+      let b = bD[i];
+      if (b.itemId) {
+        this.products.push({
+          cacheRef: b,
+          blockId: i,
+          itemId: b.itemId,
+          itemIndex: b.itemIndex,
+          title: b.itemTitle,
+          itemCount: b.itemCount,
+          desc: b.itemDesc,
+          price: b.itemPrice
+        });
+      }
+    }
+
+    this.products.sort((a, b) => {
+      if (a.itemIndex > b.itemIndex)
+        return 1;
+      if (a.itemIndex < b.itemIndex)
+        return -1;
+      return 0;
+    });
+
+    for (let c = 0, l = this.products.length; c < l; c++)
+      this.productShowPriceAndImage(c);
+  }
+  productShowPriceAndImage(index) {
+    let product = this.products[index];
+
+    if (product.priceShown)
+      return;
+
+    product.priceShown = true;
+    if (!product.priceShape) {
+      product.priceShape = 'priceshape' + product.itemId;
+      product.titleShape = 'titleshape' + product.itemId;
+
+      let priceShapeData = {
+        title: product.priceShape,
+        materialName: 'decolor: .5,.1,.1',
+        shapeType: 'text',
+        textFontFamily: 'Arial',
+        textText: product.desc,
+        textDepth: '.1',
+        textSize: '100'
+      };
+
+      gAPPP.a.modelSets['shape'].createWithBlobString(priceShapeData).then(results => {});
+      gAPPP.a.modelSets['shape'].createWithBlobString({
+        title: product.titleShape,
+        materialName: 'decolor: .1,.5,.5',
+        shapeType: 'text',
+        textFontFamily: 'Times',
+        textText: product.title,
+        textDepth: '.3',
+        textSize: 100
+      }).then(results => {});
+    }
+
+    gAPPP.a.modelSets['blockchild'].createWithBlobString({
+      parentKey: product.blockId,
+      childType: 'shape',
+      childName: product.priceShape,
+      inheritMaterial: false
+    }).then(childResults => {
+      product.priceBlockChildKey = childResults.key;
+      gAPPP.a.modelSets['frame'].createWithBlobString({
+        parentKey: product.priceBlockChildKey,
+        positionX: '',
+        positionY: '2',
+        positionZ: '',
+        rotationX: '',
+        rotationY: '180deg',
+        rotationZ: '-90deg',
+        scalingX: '',
+        scalingY: '',
+        scalingZ: '',
+        visibility: '',
+        frameOrder: 10,
+        frameTime: 0
+      }).then(fResults => {});
+    });
+    gAPPP.a.modelSets['blockchild'].createWithBlobString({
+      parentKey: product.blockId,
+      childType: 'shape',
+      childName: product.titleShape,
+      inheritMaterial: false
+    }).then(childResults => {
+      product.titleBlockChildKey = childResults.key;
+      gAPPP.a.modelSets['frame'].createWithBlobString({
+        parentKey: product.titleBlockChildKey,
+        positionX: '',
+        positionY: '3',
+        positionZ: '',
+        rotationX: '',
+        rotationY: '180deg',
+        rotationZ: '-90deg',
+        scalingX: '',
+        scalingY: '',
+        scalingZ: '',
+        visibility: '',
+        frameOrder: 10,
+        frameTime: 0
+      }).then(fResults => {});
+    });
   }
 }
--- public/demo/gdemoapp.js
+++ public/demo/gdemoapp.js
@@ -46,8 +46,6 @@ class gDemoApp extends gAppSuper {
 
       this.mV = new cViewDemo();
       this._updateApplicationStyle();
-
-      this.updateProducts();
     };
 
     document.querySelector('.choice-button-clear').addEventListener('click', () => this.hideBasketGoods());
@@ -61,40 +59,6 @@ class gDemoApp extends gAppSuper {
     document.getElementById('week-picker-select').addEventListener('input', () => this.changeSelectedWeek());
     document.getElementById('week-picker-select').value = workspaceCode;
 
-  }
-  updateProducts() {
-    if (this.productsUpdated)
-      return;
-
-    this.productsUpdated = true;
-    let bD = gAPPP.a.modelSets['block'].fireDataValuesByKey;
-    this.products = [];
-    for (let i in bD) {
-      let b = bD[i];
-      if (b.itemId) {
-        this.products.push({
-          cacheRef: b,
-          blockId: i,
-          itemId: b.itemId,
-          itemIndex: b.itemIndex,
-          title: b.itemTitle,
-          itemCount: b.itemCount,
-          desc: b.itemDesc,
-          price: b.itemPrice
-        });
-      }
-    }
-
-    this.products.sort((a, b) => {
-      if (a.itemIndex > b.itemIndex)
-        return 1;
-      if (a.itemIndex < b.itemIndex)
-        return -1;
-      return 0;
-    });
-
-    for (let c = 0, l = this.products.length; c < l; c++)
-      this.productShowPriceAndImage(c);
   }
   changeSelectedWeek() {
     let projCode = document.getElementById('week-picker-select').value;
@@ -160,86 +124,6 @@ class gDemoApp extends gAppSuper {
     /*
      */
 
-  }
-  productShowPriceAndImage(index) {
-    let product = this.products[index];
-
-    if (product.priceShown)
-      return;
-
-    product.priceShown = true;
-    if (!product.priceShape) {
-      product.priceShape = 'priceshape' + product.itemId;
-      product.titleShape = 'titleshape' + product.itemId;
-
-      let priceShapeData = {
-        title: product.priceShape,
-        materialName: 'decolor: .5,.1,.1',
-        shapeType: 'text',
-        textFontFamily: 'Arial',
-        textText: product.price,
-        textDepth: '.1',
-        textSize: '100'
-      };
-
-      gAPPP.a.modelSets['shape'].createWithBlobString(priceShapeData).then(results => {});
-      gAPPP.a.modelSets['shape'].createWithBlobString({
-        title: product.titleShape,
-        materialName: 'decolor: .1,.5,.5',
-        shapeType: 'text',
-        textFontFamily: 'Times',
-        textText: product.title,
-        textDepth: '.3',
-        textSize: 100
-      }).then(results => {});
-    }
-
-    gAPPP.a.modelSets['blockchild'].createWithBlobString({
-      parentKey: product.blockId,
-      childType: 'shape',
-      childName: product.priceShape,
-      inheritMaterial: false
-    }).then(childResults => {
-      product.priceBlockChildKey = childResults.key;
-      gAPPP.a.modelSets['frame'].createWithBlobString({
-        parentKey: product.priceBlockChildKey,
-        positionX: '',
-        positionY: '2',
-        positionZ: '',
-        rotationX: '',
-        rotationY: '180deg',
-        rotationZ: '-90deg',
-        scalingX: '',
-        scalingY: '',
-        scalingZ: '',
-        visibility: '',
-        frameOrder: 10,
-        frameTime: 0
-      }).then(fResults => {});
-    });
-    gAPPP.a.modelSets['blockchild'].createWithBlobString({
-      parentKey: product.blockId,
-      childType: 'shape',
-      childName: product.titleShape,
-      inheritMaterial: false
-    }).then(childResults => {
-      product.titleBlockChildKey = childResults.key;
-      gAPPP.a.modelSets['frame'].createWithBlobString({
-        parentKey: product.titleBlockChildKey,
-        positionX: '',
-        positionY: '3',
-        positionZ: '',
-        rotationX: '',
-        rotationY: '180deg',
-        rotationZ: '-90deg',
-        scalingX: '',
-        scalingY: '',
-        scalingZ: '',
-        visibility: '',
-        frameOrder: 10,
-        frameTime: 0
-      }).then(fResults => {});
-    });
   }
   showBasketGood(name) {
     let frames =
