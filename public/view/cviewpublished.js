711cce9afea85ae742cabcdee2d3496a94f4d59a
HuskerSam <sam.huelsdonk@gmail.com>
2019-04-28 12:30:38 -0500

    /view fixed up - first wave of refactore related to mdl example done

--- public/view/cviewpublished.js
+++ public/view/cviewpublished.js
@@ -76,4 +76,50 @@ class cViewPublished extends bView {
 
     fldList.innerHTML = fieldOptions;
   }
+  _canvasPanelTemplate() {
+    return `<canvas class="popup-canvas"></canvas>
+    <div class="video-overlay">
+      <video></video>
+    </div>
+    <div class="canvas-actions">
+      <div class="canvas-play-bar">
+        <div class="scene-options-panel" style="display:none;">
+          <div class="scene-fields-container">
+          </div>
+          <div class="render-log-wrapper" style="display:none;">
+            <button class="btn-sb-icon log-clear"><i class="material-icons">clear_all</i></button>
+            <textarea class="render-log-panel" spellcheck="false"></textarea>
+            <div class="fields-container" style="display:none;"></div>
+          </div>
+          <br>
+          <button class="btn-sb-icon stop-button"><i class="material-icons">stop</i></button>
+          <button class="btn-sb-icon video-button"><i class="material-icons">fiber_manual_record</i></button>
+          <button class="btn-sb-icon download-button"><i class="material-icons">file_download</i></button>
+          <button class="btn-sb-icon show-hide-log"><i class="material-icons">info_outline</i></button>
+        </div>
+        <br>
+        <button class="btn-sb-icon scene-options" style="clear:both;"><i class="material-icons">settings_brightness</i></button>
+        <button class="btn-sb-icon play-button"><i class="material-icons">play_arrow</i></button>
+        <button class="btn-sb-icon pause-button"><i class="material-icons">pause</i></button>
+        <div class="run-length-label"></div>
+        <input class="animate-range" type="range" step="any" value="0" min="0" max="100" />
+        <div class="camera-options-panel" style="display:inline-block;">
+          <select class="camera-select" style=""></select>
+          <div style="display:inline-block;">
+            <div class="camera-slider-label">Radius</div>
+            <input class="camera-select-range-slider" type="range" step="any" min="1" max="300" />
+          </div>
+          <div style="display:inline-block;">
+            <div class="camera-slider-label">FOV</div>
+            <input class="camera-select-range-fov-slider" type="range" step=".01" min="-1" max="2.5" value=".8" />
+          </div>
+          <div style="display:inline-block;">
+            <div class="camera-slider-label">Height</div>
+            <input class="camera-select-range-height-slider" type="range" step=".25" min="-15" max="40" />
+          </div>
+          <div class="fields-container" style="float:left"></div>
+        </div>
+      </div>
+    </div>`;
+  }
 }
--- public/view/gpublishapp.js
+++ public/view/gpublishapp.js
@@ -2,8 +2,6 @@
 class gPublishApp extends gAppSuper  {
   constructor() {
     super();
-    this.a = new gAuthorization();
-    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
     this.a.signInAnon();
   }
   profileReadyAndLoaded() {
@@ -38,4 +36,50 @@ class gPublishApp extends gAppSuper  {
       this._updateApplicationStyle();
     };
   }
+  _loginPageTemplate(title = `Dynamic Reality App`) {
+    return `<div id="firebase-app-login-page" style="display:none;">Loading...</div>`;
+  }
+  _fullScreenPageLayout() {
+    return `<div id="firebase-app-main-page" style="display:none;">
+  <div id="renderLoadingCanvas" style="display:none;"><br><br>LOADING...</div>
+  <div class="popup-canvas-wrapper main-canvas-wrapper"></div>
+  <button id="user-profile-dialog-reset-button">Reset Options</button>
+  <button id="publish-settings-button" style='display:none' class="btn-sb-icon"><i class="material-icons">settings_brightness</i></button>
+  <a href="/" id="change-view-anchor">Change View</a>
+  <div id="publish-profile-panel" style="display:none;">
+    <div id="value-set-panel">
+      <label><span>Element</span>
+      <select id="element-type-to-edit">
+        <option>Block</option>
+        <option>BlockChild</option>
+        <option>Shape</option>
+        <option>Mesh</option>
+        <option>Texture</option>
+        <option>Material</option>
+        <option>Frame</option>
+      </select>
+      </label>
+      <br>
+      <label><span>ID</span><input id="element-id-to-edit" type="text" list="elementidlist" /></label>
+      <br>
+      <label><span>Field</span><input id="field-name-to-edit" type="text" list="fieldnamelist" /></label>
+      <br>
+      <label><span>Value</span><input id="value-to-edit" type="text" /></label>
+      <br>
+      <button id="button-to-edit">Set</button>
+    </div>
+
+    <div class="fields-container"></div>
+  </div>
+</div>`;
+  }
+  _initAuthUI() {
+    let div = document.createElement('div');
+    div.innerHTML = this._loginPageTemplate('eXtended Reality Viewer');
+    div = div.firstChild;
+    document.body.insertBefore(div, document.body.firstChild);
+    this.updateAppLayout();
+    this.__initFormHandlers();
+  }
+  __initFormHandlers() {}
 }
