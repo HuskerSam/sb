c90e5c037188da96e391df11b542585694b3c20c
HuskerSam <sam.huelsdonk@gmail.com>
2019-04-28 12:18:54 -0500

    updated /demo

--- public/demo/gdemoapp.js
+++ public/demo/gdemoapp.js
@@ -2,8 +2,6 @@
 class gDemoApp extends gAppSuper {
   constructor() {
     super();
-    this.a = new gAuthorization();
-    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
     this.a.signInAnon();
   }
   profileReadyAndLoaded() {
@@ -26,4 +24,39 @@ class gDemoApp extends gAppSuper {
       this._updateApplicationStyle();
     };
   }
+  _loginPageTemplate(title = `Dynamic Reality App`) {
+    return `<div id="firebase-app-login-page" style="display:none;">Loading...</div>`;
+  }
+  _fullScreenPageLayout() {
+    return `<div id="firebase-app-main-page" style="display:none;">
+      <div id="renderLoadingCanvas" style="display:none;"><br><br>LOADING...</div>
+      <div class="popup-canvas-wrapper main-canvas-wrapper"></div>
+
+      <div class="user-options-panel">
+        <button class="choice-button-one" style="display:none;">&nbsp;</button>
+        <button class="choice-button-two" style="display:none;">&nbsp;</button>
+        <button class="choice-button-three" style="display:none;">&nbsp;</button>
+        <button class="choice-button-four" style="display:none;">&nbsp;</button>
+      </div>
+      <div class="cart-total">
+        <select id="workspaces-select"></select>
+        <button class="choice-button-clear cart-submit">Checkout</button>
+        <div class="cart-item-total">$ 0.00</div>
+        <div class="cart-contents">
+        </div>
+        <div style="text-align:center;">
+          <button id="cart-contents-more-button">More</button>
+        </div>
+      </div>
+    </div>`;
+  }
+  _initAuthUI() {
+    let div = document.createElement('div');
+    div.innerHTML = this._loginPageTemplate('eXtended Reality Grafter');
+    div = div.firstChild;
+    document.body.insertBefore(div, document.body.firstChild);
+    this.updateAppLayout();
+    this.__initFormHandlers();
+  }
+  __initFormHandlers() {}
 }
