825cf22f7f3615cd2d3dfd492d28e7eff879e444
HuskerSam <sam.huelsdonk@gmail.com>
2019-05-19 15:10:11 -0500

    removed functions (unused for now) from project

--- functions/index.js
+++
@@ -1,6 +0,0 @@
-const functions = require('firebase-functions');
-const firebaseAdmin = require('firebase-admin');
-const swaggerAPI = require('./swagger/swaggerapi');
-firebaseAdmin.initializeApp();
-
-exports.api = functions.https.onRequest(swaggerAPI);
