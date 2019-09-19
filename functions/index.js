6bc8e90d3bca0840e3b996e2df99568cabb4c0e5
Sam H <sam.huelsdonk@gmail.com>
2019-09-19 10:45:09 -0500

    added email submit

---
+++ functions/index.js
@@ -0,0 +1,25 @@
+var functions = require('firebase-functions');
+var admin = require("firebase-admin");
+
+admin.initializeApp();
+
+exports.submitEmailToList = functions.https.onRequest((req, res) => {
+  if (req.method === 'POST') {
+    if (!req.query.email)
+      return res.status(200).send('{ empty email }');
+
+    let email = req.query.email.replace(/[[\]{}()*+?.\\^$|]/g, '');
+    return admin.database().ref(`/userNewsletter/emails`).update({
+        [email]: {
+          when: new Date().toISOString(),
+          fullEmail: req.query.email
+        }
+      })
+      .then(results => res.status(200).send('{ "success": true }'))
+      .catch(e => {
+        res.status(500).send(e);
+        console.log(e);
+      });
+  }
+  res.send("Post Only");
+});
