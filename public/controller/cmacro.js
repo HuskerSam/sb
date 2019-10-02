e9129172547b52fe02bff2e05f6c117145d89d63
sam.huelsdonk@gmail.com <sam.huelsdonk@gmail.com>
2019-10-02 09:38:32 -0500

    ui iteration

--- public/controller/cmacro.js
+++ public/controller/cmacro.js
@@ -269,8 +269,8 @@ class cMacro {
       <label><span>depth</span><input type="text" class="depth" value="1" /></label>
     </div>
     <div class="scene-block-add-options">
-      <label><input type="radio" class="sceneaddtype skyboxtemplatetype" data-type="skyboxscenefeatures" name="sceneaddtype" checked /><span>Skybox Format</span></label>
-      <label><input type="radio" class="sceneaddtype buildingtemplatetype" data-type="buildingscenefeatures" name="sceneaddtype" /><span>Building Format</span></label>
+      <label><input type="radio" class="sceneaddtype skyboxtemplatetype" data-type="skyboxscenefeatures" name="sceneaddtype" checked /><span>Skybox</span></label>
+      <label><input type="radio" class="sceneaddtype buildingtemplatetype" data-type="buildingscenefeatures" name="sceneaddtype" /><span>Building</span></label>
       <div class="skyboxscenefeatures">
         <label><span>Skybox Size</span><input type="text" class="skyboxsize" value="400" /></label>
         <br>
