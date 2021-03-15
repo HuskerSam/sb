fc3fdcdc15735ab56120696b72215fb0a8407441
sam.huelsdonk@gmail.com <sam.huelsdonk@gmail.com>
2021-03-15 09:48:43 -0500

    home page iteration

--- public/display/cviewdemo.js
+++ public/display/cviewdemo.js
@@ -1236,7 +1236,7 @@ class cViewDemo extends bView {
               </select>
               <br>
               <button id="enable_vr_canvas_btn">Enable VR</button>
-              <a style="line-height:1.5em;font-size:1.5em;" href="/retail">About...</a>
+              <a style="line-height:1.5em;font-size:1.5em;" href="/retail/">About...</a>
               <br>
               <select id="workspaces-select"></select><div class="fields-container"></div>
             </div>
