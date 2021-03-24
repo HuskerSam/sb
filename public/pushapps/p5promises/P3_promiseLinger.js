3732c93ce0154a181cf8da164eeff8463478c737
Sam <sam.huelsdonk@gmail.com>
2021-03-24 16:45:08 -0500

    iteration

--- public/pushapps/p5promises/P3_promiseLinger.js
+++
@@ -1,7 +0,0 @@
-// point is to show the promise body runs without calling .then() - but still lingers unresolved
-
-let lingerPromise = new Promise(() => {
-  console.log('runs body but Promise lingers');
-});
-
-console.log('promise state', lingerPromise);
