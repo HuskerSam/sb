59cbeadea8d1e73b4e7090c5017b2113c566d6e9
Sam <sam.huelsdonk@gmail.com>
2021-05-23 16:33:56 -0500

    iteration

--- public/controller/cchat.js
+++ public/controller/cchat.js
@@ -12,23 +12,20 @@ class cChat {
 
   }
   template() {
-    return `<div class="shape_and_text_block_options">
-      <span>Line 1</span>
+    return `<div class="cchat_wrapper">
+      Message<br>
       <input type="text" class="texttext" value="My Message" />
       <br>
-      <span>Line 2</span>
-      <input type="text" class="texttextline2" />
-      <br>
-      <span>Font</span>
       <input type="text" class="textfontfamily"  list="fontfamilydatalist" />
       <br>
-      <span>Open Face</span>
-      <input type="checkbox" style="width:1.25em;line-height:1.5em" class="textstroke" />
+      <label><input type="checkbox" class="textstroke" />
+      <span>Open Face</span></label>
       <br>
-      <span>Text Material</span>
       <input type="text" class="textmaterial"  list="materialdatatitlelookuplist"/>
       <br>
-      <span>Shape Type</span>
+      Shape
+      <br>
+
       <select class="createshapetype">
         <option>cube</option>
         <option>box</option>
@@ -37,12 +34,11 @@ class cChat {
         <option>sphere</option>
         <option>ellipsoid</option>
       </select>
+      <label>
+        <input type="checkbox" class="cylinderhorizontal" />
+        <span>Rotate 90°</span>
+      </label>
       <br>
-      <span>Rotate 90°</span>
-      <input type="checkbox" style="width:1.25em;line-height:1.5em" class="cylinderhorizontal" />
-      <br>
-
-      <td>Shape Material</span>
       <input type="text" class="shapematerial" list="materialdatatitlelookuplist" />
       <br>
       <button class="submit_3d_text_msg">Submit Message</button>
